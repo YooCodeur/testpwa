df68a459defd1d3aeefc8742ab63b452d69db34f
yoocodeur <yoocohen@outlook.fr>
2025-04-08 14:56:01 +0200

    ok

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+
 export default function Home() {
   return (
-    <div className="global" style={{ backgroundColor: 'red' }}>
-      <h1>Hello World</h1>
-      <p>This is a test</p>
+    <div className="global" style={{ backgroundColor: '#F4F5F6' }}>
+      <div>
+        <Image 
+          src="/images/history-in-hd-cTz5-T7voqQ-unsplash.jpg" 
+          alt="Image historique"
+          width={800}
+          height={600}
+          priority
+        />
+      </div>
     </div>
   );
 }
